956658f945db9cabbedac760dcb88a7ba3f96d0b
Milad Rahimi <info@miladrahimi.com>
2021-07-10 17:28:50 +0430

    increase test coverage

--- tests/Json/StrictJsonParserTest.php
+++ tests/Json/StrictJsonParserTest.php
@@ -3,6 +3,7 @@
 namespace MiladRahimi\Jwt\Tests\Json;
 
 use MiladRahimi\Jwt\Exceptions\JsonDecodingException;
+use MiladRahimi\Jwt\Exceptions\JsonEncodingException;
 use MiladRahimi\Jwt\Json\StrictJsonParser;
 use MiladRahimi\Jwt\Tests\TestCase;
 use Throwable;
@@ -29,6 +30,17 @@ class StrictJsonParserTest extends TestCase
         $this->assertSame($data['integer'], $decoded['integer']);
         $this->assertSame($data['true'], $decoded['true']);
         $this->assertSame($data['false'], $decoded['false']);
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function test_encode_with_invalid_input_it_should_fail()
+    {
+        $strictJson = new StrictJsonParser();
+
+        $this->expectException(JsonEncodingException::class);
+        $strictJson->encode([NAN]);
     }
 
     /**
