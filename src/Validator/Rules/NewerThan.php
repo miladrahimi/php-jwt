003c7cb8a1ba830308fa49cad3b94f7bc32afc8d
Milad Rahimi <i@miladrahimi.com>
2026-07-23 16:55:34 +0200

    refactor: refine docblocks and messages in NewerThan

--- src/Validator/Rules/NewerThan.php
+++ src/Validator/Rules/NewerThan.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 namespace MiladRahimi\Jwt\Validator\Rules;
 
 /**
- * It checks if the claim is newer than the given timestamp
+ * Checks whether the claim is newer than the given timestamp.
  */
 class NewerThan extends GreaterThan
 {
     /**
-     * @inheritDoc
+     * {@inheritDoc}
      */
     protected function message(string $name): string
     {
